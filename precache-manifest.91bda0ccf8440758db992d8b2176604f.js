ba4e3ac57e36c3ed0574548cd71f446e0b378a03
NguyenTheVinh <nguyenthevinh1002@gmail.com>
2019-07-19 09:17:33 +0700

    Updates

---
+++ precache-manifest.91bda0ccf8440758db992d8b2176604f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ed2cc75a3d660e990efafc096de755",
+    "url": "/ChattingSocket/index.html"
+  },
+  {
+    "revision": "0ccc4e6602f584022fd1",
+    "url": "/ChattingSocket/static/css/main.71bd86f1.chunk.css"
+  },
+  {
+    "revision": "a4032611c4b03e5fb91e",
+    "url": "/ChattingSocket/static/js/2.4cce7852.chunk.js"
+  },
+  {
+    "revision": "0ccc4e6602f584022fd1",
+    "url": "/ChattingSocket/static/js/main.24d50f33.chunk.js"
+  },
+  {
+    "revision": "69722eb0628acfefa024",
+    "url": "/ChattingSocket/static/js/runtime~main.7f9685b7.js"
+  }
+]);
